fa78d68a24eac50221b98b040554a42ccc08224d
ks-sasaki <fixedstar0106@gmail.com>
2019-03-07 18:56:11 +0900

    first commit

---
+++ AppSettings/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppSettings.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.WebConfig = ConfigurationManager.AppSettings["test"];
+            ViewBag.AppSettings = Startup.AppSettings["test"];
+            return View(ViewBag);
+        }
+    }
+}
